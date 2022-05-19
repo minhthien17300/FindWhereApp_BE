650e9f1d4e6634b4e7c22effb0fc242d7a6298ea
minhthien17300 <minhthien17300@gmail.com>
2022-05-19 22:03:06 +0700

    dsjds

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use(cors());
-
+//hahahah
 
 app.use(route);
 app.get('/healCheck', (req, res) => res.status(200).json({hello : 'Welcome to ReviewGame'}))
